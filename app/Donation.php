7ea882fcff964a0a003e4eef7755ebc315979871
unknown <erickdarosilva@gmail.com>
2018-11-05 01:41:44 -0200

    Project Commit

---
+++ app/Donation.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Donation extends Model
+{
+    public function user(){
+      return $this->belongsTo(User::class);
+    }
+
+    public function donator(){
+      return $this->belongsTo(Donator::class);
+    }
+  
+}
